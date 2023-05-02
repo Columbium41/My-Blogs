e17892001ffa0704e6bf7858637d9b1b872be126
Charley Liu <charleyliu716@gmail.com>
2023-05-02 14:39:33 -0400

    Modified route name for root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  get "/", to: "main#index"
+  root to: "main#index"
   
   get "/about", to: "about#index"
 end

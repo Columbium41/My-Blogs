6558b11502747fd0040eef2d3efd01ca5e65b712
Charley Liu <charleyliu716@gmail.com>
2023-05-03 14:16:32 -0400

    Added sign up feature with error flash messages

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,30 @@ class RegistrationsController < ApplicationController
         @user = User.new
     end
 
-    def create 
-        render plain: "Thanks!"
+    def create
+        @user = User.new(user_params)
+
+        # Check if the user was successfully saved to the database or not
+        if @user.save
+            redirect_to root_path, notice: "Successfully created account"
+        else
+            error_msg = "Sign Up Failed"
+
+            if @user.errors.any?
+                @user.errors.full_messages.each do |message|
+                    error_msg += "<br>#{message}"
+                end
+            end
+
+            flash[:alert] = error_msg.html_safe
+            render :new
+        end
+    end
+
+    private
+
+    def user_params 
+        # require user params and only permit email, password, and password confirmation
+        params.require(:user).permit(:email, :password, :password_confirmation)
     end
 end

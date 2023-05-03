cf1cdc28ac5763652ec28b0382dac71552dc03c3
Charley Liu <charleyliu716@gmail.com>
2023-05-03 17:12:40 -0400

    Added additional error checking for sign-in/sign-up forms and created a model to track the current user/session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    # Run before other methods
+    before_action :set_current_user
+
+    def set_current_user
+        # Set current user if a valid session is found
+        if session[:user_id]
+            Current.user = User.find_by(id: session[:user_id])
+        end
+    end
 end

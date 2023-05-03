38d9687152aef2b06b70f98c4133cde24edaeaed
Charley Liu <charleyliu716@gmail.com>
2023-05-03 16:10:42 -0400

    Added sign in page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+    def new 
+        
+    end
+
+    def create 
+        user = User.find_by(email: params[:email])
+
+        # check if user exists and if the given password matches
+        if user.present? && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to root_path, notice: "Successfully logged in"
+        else
+            flash[:alert] = "Invalid email or password"
+            render :new
+        end
+    end
+
     def destroy 
         # remove the session user id
         session[:user_id] = nil

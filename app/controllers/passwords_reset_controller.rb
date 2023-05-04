d0a763edc97b155b1023f0560e8ce43d234e1013
Charley Liu <charleyliu716@gmail.com>
2023-05-04 14:49:32 -0400

    Added forgot password

--- app/controllers/passwords_reset_controller.rb
+++ app/controllers/passwords_reset_controller.rb
@@ -12,4 +12,35 @@ class PasswordsResetController < ApplicationController
 
         redirect_to root_path, notice: "Successfully sent password reset link in email"
     end
+
+    def edit 
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+        redirect_to sign_in_path, alert: "Your token has expired! Please try again"
+    end
+
+    def update
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+
+        if @user.update(password_params)
+            redirect_to sign_in_path, notice: "Your password was successfully reset. Please sign in."
+        else
+            error_msg = "Password Reset Failed"
+
+            if @user.errors.any?
+                @user.errors.full_messages.each do |message|
+                    error_msg += "<br>#{message}"
+                end
+            end
+
+            flash[:alert] = error_msg.html_safe
+            render :edit
+        end
+    end
+
+    private
+
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
+    end
 end

50a54f4888ac284e3b5e98445aabe355ccb1cbff
Charley Liu <charleyliu716@gmail.com>
2023-05-08 09:21:01 -0400

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,6 @@ class User < ApplicationRecord
     has_secure_password
 
     # Validate that the user entered in an actual email address
-    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
+    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }, uniqueness: true
+    validates :password_confirmation, presence: true
 end
